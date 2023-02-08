98efd0fcb783c4c81201af38f9e23e33079fab21
Zaakir <rubinkazan@godkiller.localdomain>
2023-02-08 18:04:47 +0000

    feature: updated icon, refactored checkbox, added surprise for reaching 100

--- src/components/CurriculumCard.tsx
+++ src/components/CurriculumCard.tsx
@@ -10,12 +10,40 @@ import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import FlagIcon from '@mui/icons-material/Flag';
 import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
-
+import { progressTitles, blackArrowStyle } from '../util/constants';
 import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 import DoneIcon from '@mui/icons-material/Done';
+import ConfettiExplosion from 'react-confetti-explosion';
 
 const Curriculum: React.FC<Course> = ({ title, body, term }) => {
+  const [checkedState, setCheckedState] = useState(new Array(4).fill(false));
+  const [progress, setProgress] = useState(0);
+  const [currentSum, setCurrentSum] = useState(0);
+  const [isExploding, setIsExploding] = React.useState(false);
+  const handleOnChange = (check: number) => {
+    const updatedCheckedState = checkedState.map((item, index: number) =>
+      index === check ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
+
+    const totalProgress = updatedCheckedState.reduce(
+      (sum, currentState) => {
+        if (currentState === true) {
+          setCurrentSum(sum + 25);
+          return sum + 25;
+        }
+        return sum;
+      },
+      0
+    );
+
+    setProgress(totalProgress);
+  };
+
   return (
+    <div>
+          { currentSum === 100 ? <ConfettiExplosion/> :
     <Card style={{ margin: "25px", border: "solid 2px", boxShadow: "13px 10px", width: "400px" }}>
       <CardContent>
         <Grid container>
@@ -26,110 +54,67 @@ const Curriculum: React.FC<Course> = ({ title, body, term }) => {
             <Typography style={{ fontWeight: "600", fontSize: "20px" }}>
               {title}
             </Typography>
-                    <Box sx={{ position: 'relative', display: 'inline-flex',paddingTop: '25px' }}>
-          <CircularProgress variant="determinate" value={Math.floor(Math.random() * 100)} style={{ color: "maroon" ,paddingLeft:"20px"}} size="4.5rem"/>
-          <Box
-            sx={{
-              top: 10,
-              left: 2.5,
-              bottom: 0,
-              right: 0,
-              position: 'absolute',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Typography
-              variant="caption"
-              component="div"
-              color="black"
-              style={{fontSize:"20px"}}
-            >{`${Math.floor(Math.random() * 100)}`}</Typography>
-          </Box>
-        </Box>
+            <Box sx={{ position: 'relative', display: 'inline-flex', paddingTop: '25px' }}>
+              <CircularProgress variant="determinate" value={progress} style={{ color: "maroon", paddingLeft: "20px" }} size="4.5rem" />
+              <Box
+                sx={{
+                  top: 10,
+                  left: 2.5,
+                  bottom: 0,
+                  right: 0,
+                  position: 'absolute',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Typography
+                  variant="caption"
+                  component="div"
+                  color="black"
+                  style={{ fontSize: "20px" }}
+                >{progress}</Typography>
+              </Box>
+            </Box>
           </Grid>
 
           <Grid item>
-            <Typography color="text.secondary" style={{}}>
+            <Typography color="text.secondary">
               Track Progress
             </Typography>
-
-            <Tooltip arrow title="Started" componentsProps={{
-              tooltip: {
-                sx: {
-                  bgcolor: 'common.black',
-                  '& .MuiTooltip-arrow': {
-                    color: 'common.black',
-                  },
-                },
-              }
-            }}>
-              <Checkbox style={{ color: "green" }} icon={<DoneOutlineIcon />} checkedIcon={<DoneIcon />} />
+            {progressTitles.map((title, index) => {
+              return (
+                <Tooltip arrow title={title} componentsProps={blackArrowStyle}>
+                  <Checkbox style={{ color: "green" }} icon={<DoneOutlineIcon />} checkedIcon={<DoneIcon />} checked={checkedState[index]} onChange={() => handleOnChange(index)} />
+                </Tooltip>
+              );
+            })}
+            <Tooltip arrow title="Finish Challenge" componentsProps={blackArrowStyle}>
+              <Checkbox style={{ color: "brown" }} icon={<FlagOutlinedIcon />} checkedIcon={<FlagIcon />} checked={checkedState[3]} onChange={() => handleOnChange(3)} />
             </Tooltip>
-            <Tooltip arrow title="Engaged with resources" componentsProps={{
-              tooltip: {
-                sx: {
-                  bgcolor: 'common.black',
-                  '& .MuiTooltip-arrow': {
-                    color: 'common.black',
-                  },
-                },
-              }
-            }}>
-              <Checkbox style={{ color: "green" }} icon={<DoneOutlineIcon />} checkedIcon={<DoneIcon />} />
+            <Typography color="text.secondary" style={{ paddingTop: "10px" }}>
+              Resources
+            </Typography>
+            <Tooltip title={body[0].video.title} componentsProps={blackArrowStyle} arrow={true}>
+              <IconButton href={body[0].video.url} target={"_blank"}>
+                <PlayCircleOutlineIcon />
+              </IconButton>
             </Tooltip>
-            <Tooltip arrow title="Practiced Technique" componentsProps={{
-              tooltip: {
-                sx: {
-                  bgcolor: 'common.black',
-                  '& .MuiTooltip-arrow': {
-                    color: 'common.black',
-                  },
-                },
-              }
-            }}>
-              <Checkbox style={{ color: "green" }} icon={<DoneOutlineIcon />} checkedIcon={<DoneIcon />} />
+            <Tooltip title={body[0].reading.title} componentsProps={blackArrowStyle} arrow={true} >
+              <IconButton href={body[0].reading.url} target={"_blank"}>
+                <AutoStoriesIcon />
+              </IconButton>
             </Tooltip>
-            <Tooltip arrow title="Finish Challenge" componentsProps={{
-              tooltip: {
-                sx: {
-                  bgcolor: 'common.black',
-                  '& .MuiTooltip-arrow': {
-                    color: 'common.black',
-                  },
-                },
-              }
-            }}>
-              <Checkbox style={{ color: "brown" }} icon={<FlagOutlinedIcon />} checkedIcon={<FlagIcon />} />
+            <Tooltip title={body[0].challenge.title} componentsProps={blackArrowStyle}>
+              <IconButton href={body[0].challenge.url} target={"_blank"}>
+                <TrendingUpIcon />
+              </IconButton>
             </Tooltip>
-
-        <Typography color="text.secondary" style={{paddingTop:"10px"}}>
-          Resources
-        </Typography>
-        <Tooltip title={body[0].video.title} arrow={true}>
-          <IconButton href={body[0].video.url} target={"_blank"}>
-            <PlayCircleOutlineIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title={body[0].reading.title} arrow={true} >
-          <IconButton href={body[0].reading.url} target={"_blank"}>
-            <AutoStoriesIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title={body[0].challenge.title}>
-          <IconButton href={body[0].challenge.url} target={"_blank"}>
-            <TrendingUpIcon />
-          </IconButton>
-        </Tooltip>
           </Grid>
-
         </Grid>
-
-
       </CardContent>
-
-    </Card>
+    </Card>}
+    </div>
   )
 }
 
---
+++ src/util/constants.ts
@@ -0,0 +1,12 @@
+export const progressTitles = ["Started", "Engaged with resources", "Practiced Technique"]
+
+export const blackArrowStyle = {
+  tooltip: {
+    sx: {
+      bgcolor: 'common.black',
+      '& .MuiTooltip-arrow': {
+        color: 'common.black',
+      },
+    },
+  }
+}
