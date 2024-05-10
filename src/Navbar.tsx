86b243be8dc1bf1f383e5fec7656795120acec0e
zaakir <90780598+zaakirio@users.noreply.github.com>
2024-05-10 23:10:41 +0100

    Update Navbar.tsx

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -18,19 +18,13 @@ const NavBar: React.FC<NavBarProps> = ({
     onRegisterClick,
 }) => (
     <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        // <div >
-        //     <Button className="btn" onClick={onHomeClick}><h1 className="font-black">Home</h1></Button>
-        //     <Button className="btn" onClick={onAboutClick}><h1 className="font-black">About US</h1></Button>
-        // </div>
+
         <Grid style={{ margin: 20 }}>
         <h1 style={{ color: "black", alignItems: "center" }} >
           <strong className="c">Solo Art Journey Tracker </strong>
         </h1>
       </Grid>
-        // <div>
-        //     <Button className="btn" onClick={onSignUpClick}><h1 className="font-black">Sign Up</h1></Button>
-        //     <Button className="btn" onClick={onRegisterClick}><h1 className="font-black">Register</h1></Button>
-        // </div>
+
     </nav>
 );
 
